73824e51baa9a0e6c64dd4cbcebb1ca7fcc60fe8
sabhyata-aryal <sabhyata.aryal01@gmail.com>
2025-10-17 01:52:31 +0545

    Update ALLOWED_HOSTS

--- ntproj/settings.py
+++ ntproj/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
-ALLOWED_HOSTS = ["naturetouch-6ztf.onrender.com"]
+ALLOWED_HOSTS = ["naturetouch.onrender.com"]
 
 
 from pathlib import Path
